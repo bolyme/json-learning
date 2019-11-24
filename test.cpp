87929dad817636c484dca0cc870432ebf9365f62
bolyme <42831477+bolyme@users.noreply.github.com>
2019-11-24 10:20:42 +0800

    Update test.cpp
    annotate the stringify

--- test.cpp
+++ test.cpp
@@ -239,45 +239,47 @@ do{\
 	v.free();\
 	free(json2);\
 }while(0)
+//
+//static void test_stringify() {
+//	TEST_ROUNDTRIP("null");
+//	TEST_ROUNDTRIP("true");
+//	TEST_ROUNDTRIP("false");
+//
+//	TEST_ROUNDTRIP("0");
+//	TEST_ROUNDTRIP("-0");
+//	TEST_ROUNDTRIP("1");
+//	TEST_ROUNDTRIP("-1");
+//	TEST_ROUNDTRIP("1.5");
+//	TEST_ROUNDTRIP("-1.5");
+//	TEST_ROUNDTRIP("3.25");
+//	TEST_ROUNDTRIP("1e+20");
+//	TEST_ROUNDTRIP("1.234e+20");
+//	TEST_ROUNDTRIP("1.234e-20");
+//
+//	TEST_ROUNDTRIP("1.0000000000000002"); /* the smallest number > 1 */
+//	TEST_ROUNDTRIP("4.9406564584124654e-324"); /* minimum denormal */
+//	TEST_ROUNDTRIP("-4.9406564584124654e-324");
+//	TEST_ROUNDTRIP("2.2250738585072009e-308");  /* Max subnormal double */
+//	TEST_ROUNDTRIP("-2.2250738585072009e-308");
+//	TEST_ROUNDTRIP("2.2250738585072014e-308");  /* Min normal positive double */
+//	TEST_ROUNDTRIP("-2.2250738585072014e-308");
+//	TEST_ROUNDTRIP("1.7976931348623157e+308");  /* Max double */
+//	TEST_ROUNDTRIP("-1.7976931348623157e+308");
+//
+//	TEST_ROUNDTRIP("\"\"");
+//	TEST_ROUNDTRIP("\"Hello\"");
+//	TEST_ROUNDTRIP("\"Hello\\nWorld\"");
+//	TEST_ROUNDTRIP("\"\\\" \\\\ / \\b \\f \\n \\r \\t\"");
+//	TEST_ROUNDTRIP("\"Hello\\u0000World\"");
+//	TEST_ROUNDTRIP("\"\\u20AC\"");
+//
+//	TEST_ROUNDTRIP("[]");
+//	TEST_ROUNDTRIP("[null,false,true,123,\"abc\",[1,2,3]]");
+//
+//	TEST_ROUNDTRIP("{}");
+//	TEST_ROUNDTRIP("{\"n\":null,\"f\":false,\"t\":true,\"i\":123,\"s\":\"abc\",\"a\":[1,2,3],\"o\":{\"1\":1,\"2\":2,\"3\":3}}");
+//}
 
-static void test_stringify() {
-	TEST_ROUNDTRIP("null");
-	TEST_ROUNDTRIP("true");
-	TEST_ROUNDTRIP("false");
-
-	TEST_ROUNDTRIP("0");
-	TEST_ROUNDTRIP("-0");
-	TEST_ROUNDTRIP("1");
-	TEST_ROUNDTRIP("-1");
-	TEST_ROUNDTRIP("1.5");
-	TEST_ROUNDTRIP("-1.5");
-	TEST_ROUNDTRIP("3.25");
-	TEST_ROUNDTRIP("1e+20");
-	TEST_ROUNDTRIP("1.234e+20");
-	TEST_ROUNDTRIP("1.234e-20");
-
-	TEST_ROUNDTRIP("1.0000000000000002"); /* the smallest number > 1 */
-	TEST_ROUNDTRIP("4.9406564584124654e-324"); /* minimum denormal */
-	TEST_ROUNDTRIP("-4.9406564584124654e-324");
-	TEST_ROUNDTRIP("2.2250738585072009e-308");  /* Max subnormal double */
-	TEST_ROUNDTRIP("-2.2250738585072009e-308");
-	TEST_ROUNDTRIP("2.2250738585072014e-308");  /* Min normal positive double */
-	TEST_ROUNDTRIP("-2.2250738585072014e-308");
-	TEST_ROUNDTRIP("1.7976931348623157e+308");  /* Max double */
-	TEST_ROUNDTRIP("-1.7976931348623157e+308");
-
-	TEST_ROUNDTRIP("\"\"");
-	TEST_ROUNDTRIP("\"Hello\"");
-	TEST_ROUNDTRIP("\"Hello\\nWorld\"");
-	TEST_ROUNDTRIP("\"\\\" \\\\ / \\b \\f \\n \\r \\t\"");
-	TEST_ROUNDTRIP("\"Hello\\u0001World\"");
-
-	TEST_ROUNDTRIP("[]");
-	TEST_ROUNDTRIP("[null,false,true,123,\"abc\",[1,2,3]]");
-
-	TEST_ROUNDTRIP("{}");
-	TEST_ROUNDTRIP("{\"n\":null,\"f\":false,\"t\":true,\"i\":123,\"s\":\"abc\",\"a\":[1,2,3],\"o\":{\"1\":1,\"2\":2,\"3\":3}}");
-}
 #define TEST_ERROR(error, json)\
 do{\
 	Lept_value v;\
